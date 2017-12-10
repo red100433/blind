8183d41498477bc470be8d209bb92241c5904cc9
taeshik.heo <taeshik.heo@AD01212473>
2017-12-10 16:33:48 +0900

    Initial Commit

---
+++ src/com/nts/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.nts;
+
+/**
+ * 2018 상반기 NTS Intern.
+ * First git Project 
+ * 
+ * @author taeshik.heo
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
