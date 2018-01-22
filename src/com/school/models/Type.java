2b892690a4feb3095019c18a1977f3509c08c73a
장대윤 <daeyun-jang@nhn.com>
2018-01-22 15:25:29 +0900

    성적 Controller 조회부분 구현

--- src/com/school/models/Type.java
+++ src/com/school/models/Type.java
@@ -1,9 +1,13 @@
 package com.school.models;
 
 public class Type {
+	public static final String BASIC_PATH = "C:\\Users\\USER\\Desktop\\java\\basicJava\\WebContent\\WEB-INF\\";
 	public static final String INSERT = "INSERT";
 	public static final String UPDATE = "UPDATE";
 	public static final String DELETE = "DELETE";
 	public static final String SLELCT = "SELECT";
-	public static final String BASIC_PATH = "C:\\Users\\USER\\Desktop\\java\\basicJava\\WebContent\\WEB-INF\\";
+	public static final String ALL_SELECT = "ALL_SELECT";
+	public static final String STUDENT_SELECT = "STUDENT_SELECT";
+	public static final String ALL_STUDENT_AVERAGE_SELECT = "ALL_STUDENT_AVERAGE_SELECT";
+	public static final String ALL_SUBJECT_AVERAGE_SELECT = "ALL_SUBJECT_AVERAGE_SELECT";
 }
--- src/com/school/servlet/GradeController.java
+++ src/com/school/servlet/GradeController.java
@@ -1,7 +1,11 @@
 package com.school.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,16 +14,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.school.models.Grade;
+import com.school.models.Student;
+import com.school.models.Subject;
 import com.school.models.Type;
 import com.school.service.GradeService;
+import com.school.service.StudentService;
+import com.school.service.SubjectService;
 
 public class GradeController extends HttpServlet {
 	GradeService gradeService = GradeService.getInstance();
+	StudentService studentService = StudentService.getInstance();
+	SubjectService subjectService = SubjectService.getInstance();
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		String management = req.getParameter("Manage");
 		String crud = req.getParameter("Crud");
+		String selectOption = req.getParameter("selectOption");
 
 		String name = req.getParameter("name");
 		String grade = req.getParameter("grade");
@@ -28,6 +39,7 @@ public class GradeController extends HttpServlet {
 		String changeGrade = req.getParameter("changBirth");
 		String changeSubject = req.getParameter("changeSubject");
 
+		List<String> list = Collections.emptyList();
 		switch (crud) {
 			case Type.INSERT:
 				gradeService.insert(name, subject, Integer.parseInt(grade));
@@ -38,9 +50,13 @@ public class GradeController extends HttpServlet {
 			case Type.DELETE:
 				gradeService.delete(name, subject);
 				break;
+			case Type.SLELCT:
+				list = selectOption(selectOption, name);
+				break;
 		}
-
-		List<Grade> list = gradeService.select();
+		if ("".equals(selectOption)) {
+			list = selectOption(Type.ALL_SELECT, name);
+		}
 
 		System.out.println("ManageMent Data:" + management);
 		System.out.println("Curd Data:" + crud);
@@ -51,5 +67,58 @@ public class GradeController extends HttpServlet {
 
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
 		dispatcher.forward(req, res);
+	}
+
+	private List<String> selectOption(String selectOption, String name) {
+
+		switch (selectOption) {
+			case Type.ALL_SELECT:
+				List<String> result = gradeService.select().stream()
+					.map(o -> o.toString())
+					.collect(Collectors.toList());
+
+				gradeService.select().forEach(System.out::println);
+				return result;
+			case Type.STUDENT_SELECT:
+				OptionalDouble studentAverage = getAverage(name);
+				List<String> result1 = new ArrayList<>();
+				studentAverage.ifPresent(o -> {
+					System.out.println(name + "의 평균: " + o);
+					result1.add(name + "의 평균: " + o);
+				});
+				return result1;
+			case Type.ALL_STUDENT_AVERAGE_SELECT:
+				List<String> result2 = new ArrayList<>();
+				for (Student stu : studentService.select()) {
+					OptionalDouble allStudentAverage = getAverage(stu.getStudentName());
+
+					allStudentAverage.ifPresent(
+						o -> {
+							System.out.println(stu.getStudentName() + "의 평균: " + o);
+							result2.add(stu.getStudentName() + "의 평균:" + o);
+						});
+				}
+				return result2;
+			case Type.ALL_SUBJECT_AVERAGE_SELECT:
+				List<String> result3 = new ArrayList<>();
+				for (Subject sub : subjectService.select()) {
+					OptionalDouble allSubjectAverage = getAverage(sub.getSubjectName());
+
+					allSubjectAverage.ifPresent(o -> {
+						System.out.println(sub.getSubjectName() + "의 평균: " + o);
+						result3.add(sub.getSubjectName() + "의 평균: " + o);
+					});
+				}
+				return result3;
+		}
+		return Collections.emptyList();
+
+	}
+
+	private OptionalDouble getAverage(String name) {
+		return gradeService.select().stream()
+			.filter(s -> s.getStudentName().equals(name))
+			.mapToInt(Grade::getGrade)
+			.average();
 	}
 }
