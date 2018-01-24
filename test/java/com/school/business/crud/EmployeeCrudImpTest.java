7c8a4b28cfb0dfed6e4f158467ca32f4c2fb4963
장대윤 <daeyun-jang@nhn.com>
2018-01-24 10:55:40 +0900

    Test Code 작성

---
+++ test/java/com/school/business/crud/EmployeeCrudImpTest.java
@@ -0,0 +1,31 @@
+package com.school.business.crud;
+
+public class EmployeeCrudImpTest {
+
+	//	@Before
+	//
+	//	@Test
+	//	public List<Employee> insert(List<Employee> list) {
+	//		if (list.size() != Type.LIMIT_PERSON & list.contains(temp) == false) {
+	//			list.add(temp);
+	//		}
+	//
+	//		return list;
+	//	}
+	//
+	//	@Test
+	//	public List<Employee> update(List<Employee> list, String changeName, String changeBirth) {
+	//		Employee change = new Employee(changeName, changeBirth);
+	//		if (list.contains(temp) & (list.contains(change) == false)) {
+	//			list.remove(temp);
+	//			list.add(change);
+	//		}
+	//		return list;
+	//	}
+	//
+	//	@Test
+	//	public List<Employee> delete(List<Employee> list) {
+	//		list.remove(temp);
+	//		return list;
+	//	}
+}
