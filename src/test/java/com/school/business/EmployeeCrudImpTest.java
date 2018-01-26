fd9e9a36c89eedcde7b0a8a3320a000225be653a
daeyunjang <daeyun-jang@nhn.com>
2018-01-26 09:58:46 +0900

    test code 수정

--- src/test/java/com/school/business/EmployeeCrudImpTest.java
+++ src/test/java/com/school/business/EmployeeCrudImpTest.java
@@ -12,35 +12,31 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Matchers;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import com.school.custom.EmployeeCrud;
 import com.school.models.request.EmployeeRequest;
 import com.school.models.vo.Employee;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EmployeeCrudImpTest {
 
-	@Mock
-	EmployeeCrud mockEmp;
+	@InjectMocks
+	EmployeeCrudImp mockEmp;
 
 	@Test
 	public void insertTest() {
 
 		List<Employee> insertList = new ArrayList<>();
 		insertList.add(new Employee("", ""));
+		EmployeeRequest emp = EmployeeRequest.builder()
+			.name("jjj").birth("111").changeName("").changeBirth("").build();
 
 		// 기대값(리턴값)
 		List<Employee> exfInsertList = Arrays.asList(new Employee("", ""), new Employee("jjj", "111"));
 		//함수 호출됫을때 리턴하는 값을 지정
 		when(mockEmp.insert(Matchers.anyListOf(Employee.class), anyObject())).thenReturn(exfInsertList);
-
-		insertList.add(new Employee("jjj", "111"));
-
-		EmployeeRequest emp = EmployeeRequest.builder()
-			.name("jjj").birth("111").changeName("").changeBirth("").build();
 
 		//호출
 		insertList = mockEmp.insert(insertList, emp);
@@ -48,6 +44,28 @@ public class EmployeeCrudImpTest {
 		// 기대값이랑, (assert
 		assertThat(exfInsertList, is(insertList));
 	}
+	//	@Test
+	//	public void insertTest() {
+	//
+	//		List<Employee> insertList = new ArrayList<>();
+	//		insertList.add(new Employee("", ""));
+	//
+	//		// 기대값(리턴값)
+	//		List<Employee> exfInsertList = Arrays.asList(new Employee("", ""), new Employee("jjj", "111"));
+	//		//함수 호출됫을때 리턴하는 값을 지정
+	//		when(mockEmp.insert(Matchers.anyListOf(Employee.class), anyObject())).thenReturn(exfInsertList);
+	//
+	//		insertList.add(new Employee("jjj", "111"));
+	//
+	//		EmployeeRequest emp = EmployeeRequest.builder()
+	//			.name("jjj").birth("111").changeName("").changeBirth("").build();
+	//
+	//		//호출
+	//		insertList = mockEmp.insert(insertList, emp);
+	//
+	//		// 기대값이랑, (assert
+	//		assertThat(exfInsertList, is(insertList));
+	//	}
 
 	@Test
 	public void updateTest() {
