ae53caf853ede443d38ff4df048dbfd0d33a0588
장대윤 <daeyun-jang@nhn.com>
2018-02-23 16:41:30 +0900

    부하테스트 삭제

--- src/main/java/com/nhn/blind/BlindApplication.java
+++ src/main/java/com/nhn/blind/BlindApplication.java
@@ -10,11 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class BlindApplication {
-
-	/**
-	 * netty worker Thread를 1개로 만들고 테스트 진행(부하 테스트 100개 요청할 때, 동기적으로 타는지 테스트)
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BlindApplication.class, args);
 	}
