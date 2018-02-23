1d72ce24bb5a22d826ee089f214c6a53f90bd9b4
장대윤 <daeyun-jang@nhn.com>
2018-02-23 16:38:38 +0900

    Worker Thread, CORS 삭제

--- src/main/java/com/nhn/blind/config/LoginCheckInterceptor.java
+++ src/main/java/com/nhn/blind/config/LoginCheckInterceptor.java
@@ -17,21 +17,6 @@ public class LoginCheckInterceptor implements WebFilter {
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
 
-		/**
-		 * CORS 설정
-		 */
-		//    	exchange.getResponse().getHeaders().add("Access-Control-Allow-Origin", "*");
-		//    	exchange.getResponse().getHeaders().add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-		//    	exchange.getResponse().getHeaders().add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range");
-		//        if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
-		//        	exchange.getResponse().getHeaders().add("Access-Control-Max-Age", "1728000");
-		//        	exchange.getResponse().setStatusCode(HttpStatus.NO_CONTENT);
-		//            return Mono.empty();
-		//        } else {
-		//        	exchange.getResponse().getHeaders().add("Access-Control-Expose-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range");
-		//            return chain.filter(exchange);
-		//        }
-
 		if (exchange.getRequest().getURI().getPath().equals("/signup")) {
 			return chain
 				.filter(exchange.mutate().request(exchange.getRequest().mutate().path("/signup").build()).build());
